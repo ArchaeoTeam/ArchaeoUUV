3c9d5719e5101f3deb31f32d148ff27ce415f27e
Thomas-David Griedel <T_D_Griedel@gmx.de>
2023-01-09 21:17:04 +0100

    FastAPI needed apperently

--- BOOT/gopro-control-blueos/app/main.py
+++ BOOT/gopro-control-blueos/app/main.py
@@ -2,17 +2,31 @@
 from pathlib import Path
 
 import uvicorn
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi_versioning import VersionedFastAPI, version
 from loguru import logger
 from typing import Any
 
 
-SERVICE_NAME = "GoProControl"
+SERVICE_NAME = "ExampleExtension5"
+
+app = FastAPI(
+    title="Example Extension r API",
+    description="API for an example extension that saves/loads data as files.",
+)
 
 logger.info(f"Starting {SERVICE_NAME}!")
 
-async def app() -> Any:
+app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
+
+app.mount("/", StaticFiles(directory="static",html = True), name="static")
+
+@app.get("/", response_class=FileResponse)
+async def root() -> Any:
         return "index.html"
 
 if __name__ == "__main__":
     # Running uvicorn with log disabled so loguru can handle it
-    uvicorn.run("main:app", host="0.0.0.0", port=6789, log_config=None)
+    uvicorn.run(app, host="127.0.0.1", port=6789, log_config=None)
