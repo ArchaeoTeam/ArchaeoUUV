c96db556a8903649c5c2c477d59fe1996fb6b193
Thomas-David Griedel <T_D_Griedel@gmx.de>
2023-01-09 21:11:26 +0100

    SLIMMING

--- BOOT/gopro-control-blueos/app/main.py
+++ BOOT/gopro-control-blueos/app/main.py
@@ -5,39 +5,14 @@ import uvicorn
 from loguru import logger
 from typing import Any
 
-class TextData(BaseModel):
-    data: str
 
-
-SERVICE_NAME = "ExampleExtension5"
-
-app = FastAPI(
-    title="Example Extension r API",
-    description="API for an example extension that saves/loads data as files.",
-)
+SERVICE_NAME = "GoProControl"
 
 logger.info(f"Starting {SERVICE_NAME}!")
 
-servos = {}
-
-@app.post("/setServo", status_code=status.HTTP_200_OK)
-@version(1, 0)
-async def set_servo(pin: int, pwm: float) -> Any:
-    if pin < 0 or pwm < -1 or pwm > 1:
-        raise HTTPException("Invalid values")
-    if pin not in servos:
-        servos[pin] = Servo(pin)
-    servos[pin].value = pwm
-    return "ok"
-
-app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
-
-app.mount("/", StaticFiles(directory="static",html = True), name="static")
-
-@app.get("/", response_class=FileResponse)
-async def root() -> Any:
+async def app() -> Any:
         return "index.html"
 
 if __name__ == "__main__":
     # Running uvicorn with log disabled so loguru can handle it
-    uvicorn.run(app, host="0.0.0.0", port=6789, log_config=None)
+    uvicorn.run("main:app", host="0.0.0.0", port=6789, log_config=None)
