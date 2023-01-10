26ae1d2e63ef8a01fa1ce5eeaf7169d178aeb6be
bommix <m.bommhardt@gmx.de>
2023-01-10 00:42:00 +0000

    commit

--- git.sh
+++ git.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-git add *
-git commit -m "`date`"
-git push
+#!/usr/bin/expect -f
+spawn git add *
+spawn git commit -m "commit"
+spawn git push
+expect "*?Username:*"
+send -- "bommix\r"
+expect "*?Password:*"
+send -- "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"
+send -- "\r"
+send -- "\r"
+expect eof
