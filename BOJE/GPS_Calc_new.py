c788b1a0337e8bb862236e228d7c9e4005fd64ef
Thomas-David Griedel <T_D_Griedel@gmx.de>
2023-01-17 22:53:58 +0100

    test serial nmea

--- BOJE/GPS_Calc_new.py
+++ BOJE/GPS_Calc_new.py
@@ -3,11 +3,11 @@ import time #import the Timing library for RB Pi 4 B board
 from urllib.parse import urlparse
 import requests
 import threading
-import subprocess
-import os
-import socket
+import os, sys
+import socket, serial
 import math
-import sys
+import pynmea2
+
 from osgeo.ogr import Geometry, wkbPoint
 from osgeo.osr import SpatialReference, SpatialReference, CoordinateTransformation
 
@@ -25,9 +25,8 @@ RTKLAT=0
 RTKLON=0
 
 
-
+#ENCODER CONSTANTS
 AMT22_NOP = 0 #command to read the position of the encoder
-#AMT22_NOP = hex("00A00000")
 AMT22_NOP = 0x00
 AMT22_RESET = 0x60
 AMT22_ZERO = 0x70
@@ -53,12 +52,11 @@ def update_boot_values():
    while True:
       global depth
       global compass
-      depth2 = float(requests.get(alt_url).text)
+
+      alt = float(requests.get(alt_url).text)
+
+      depth = 0.0 if alt < 0.0 else alt  #the ROV cannot fly yet
       compass = float(requests.get(compass_url).text)
-      if depth2 < 0.0:
-         depth=0.0
-      else:
-         depth=depth2
 
 def update_encoder_values():
    while True:
@@ -75,14 +73,11 @@ def update_encoder_values():
       #lenght=(((calibturns-turns)*16383)-rotation+calibtotat)/370
       #print(lenght)
 
-def rec_Boje():
-    while True:
-        #s.system("sudo stty -F /dev/ttyAMA0 115200")
-        #date = subprocess.run(["date '+%Y.%m.%d_%H:%M:%S_GPSBOJE.log'"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        #print("date")
-        #ime.sleep(1.1)
-        #s.system("sudo cat /dev/ttyAMA0 | tee /home/pi/NMEA/GPSBOJE.log | grep GNGGA")
-        time.sleep(1)
+def readSerialNMEA():
+   with serial.Serial('/dev/ttyAMA0', baudrate=115200) as ser:
+      line = ser.readline().decode('ascii', errors='replace')
+      print(line.strip())
+   
         
 def send_RTK():
     while True:
@@ -103,103 +98,101 @@ def rec_RTK():
         FIXLAT=51,07205984
         FIXLON=13,59835664
         #Prüfen ob die Eingestellte RTK Koordinate richtig sein kann
-        if abs(RTKLAT-FIXLAT) < 100
-            if abs(RTKLON-FIXLON) < 100
+        if abs(RTKLAT-FIXLAT) < 100:
+            if abs(RTKLON-FIXLON) < 100:
                RTK=True
                RTKpoint(FIXLAT-RTKLAT,FIXLON-RTKLON)
                RTKpoint.Transform(transform)
                RTKX=RTKpoint.GetX()
                RTKY=RTKpoint.GetY()
                
-            
             else: 
                RTK=False
                print("RTK fixpunkt falsch")
         else: 
            RTK=False
            print("RTK fixpunkt falsch")
-            
-        
 
-    
-        
-#start depth & compass thread
-thread_boot = threading.Thread(target=update_boot_values, args=(), daemon=True)
-thread_boot.start()
-
-#start encoder thread
-thread_encoder = threading.Thread(target=update_encoder_values, args=(), daemon=True)
-thread_encoder.start()
-
-#thread_send_RTK = threading.Thread(target=send_RTK, args=(), daemon=True)
-#thread_send_RTK.start()
-#thread_rec_RTK = threading.Thread(target=rec_RTK, args=(), daemon=True)
-#thread_rec_RTK.start()
-
-while True:
-   os.system("clear")
-   #Print boot data
-   print("Tiefe")
-   print(depth)
-   print("Kompass")
-   print(compass)
-
-   #Print encoder data
-   print("Result: " + str(result))
-   print("RAWRotation: " + str(rotation))
-   print("RAWTurns: " + str(turns))
-
-   #Print Turns
-   fturn=turns+( (rotation-4500)/16383 )
-   print("Turns: " + str(fturn))
-
-   #Convert Turns to meters
-   print("Meters: " + str(fturn/2.3806))
-   #Abstand=sqrt(Länge Kabel^2-tiefe^2)
-   #calc Correctionlenght
-   correction=math.sqrt(math.pow(fturn/2.3806,2) - math.pow(depth,2))
-   print("Correction:" + str(correction))
-   
-   #Calc with compas in UTM
-   UTMY=math.sin(compass)*correction
-   UTMX=math.cos(compass)*correction
-   
-   #winkel 0 = cosinus 1 -- x
-   #winkel 90 = sinus 1 --> y
-   print("UTMX:" + str(UTMX))
-   print("UTMY:" + str(UTMY))
-   
-   # Hier erstmal beispieldaten
-   Lat="51.035540"
-   Lon="13.735870"
-   
-   # --> Bringe die Koordinaten in gdal
-   point.AddPoint(Lat, Lon)
-   #--> Transformiere in UTM
-   point.Transform(transform)
-   #--> Bestimme Genauigkeit im schnitt 2cm pro meter 2cm bis 10m danach 2m --> 
-   Accuracy = math.sqrt((str(fturn/2.3806) * 0,02)*(str(fturn/2.3806) * 0,02) + 4)
-   if depth < 20
-      Accuracy = math.sqrt((str(fturn/2.3806) * 0,02)*(str(fturn/2.3806) * 0,02) + 0,15 *0,15)
+def getAccuracy(distance, depth):
+   #--> Bestimme Genauigkeit im schnitt 2cm pro meter 2cm bis 10m danach 2m --> TODO: Why these values?
+   if depth < 20:
+      return math.sqrt((str(distance) * 0,02)*(str(distance) * 0,02) + 0,15 * 0,15)
    else:
-      Accuracy = math.sqrt((str(fturn/2.3806) * 0,02)*(str(fturn/2.3806) * 0,02) + 2 * 2)
+      return math.sqrt((str(distance) * 0,02)*(str(distance) * 0,02) + 2 * 2)
 
-   
-   if RTK = True:
-      point.AddPoint(point.GetX()+RTKX, point.GetY()+RTKY)
-      Accuracy = Accuracy + 0,35
-   else:
-      Accuracy = Accuracy + 3
+
+def test():
+   #___________________________MAIN_______________________________        
+
+   #start depth & compass thread
+   thread_boot = threading.Thread(target=update_boot_values, args=(), daemon=True)
+   thread_boot.start()
+
+   #start encoder thread
+   thread_encoder = threading.Thread(target=update_encoder_values, args=(), daemon=True)
+   thread_encoder.start()
+
+   while True:
+      os.system("clear")
+      #Print boot data
+      print("Tiefe")
+      print(depth)
+      print("Kompass")
+      print(compass)
+
+      #Print RAW encoder data
+      print("Result: " + str(result))
+      print("RAWRotation: " + str(rotation))
+      print("RAWTurns: " + str(turns))
+
+      #Convert RAW encoder data to Wheel-Turns float
+      fturn = turns+( (rotation-4500)/16383 )
+      print("Turns: " + str(fturn))
+
+      #Convert Turns to Distance(Buoy, UUV)
+      distance = fturn/2.3806
+      print("Meters: " + str(distance))
+
+      #Calculate Offset with Pythagoras | distance² = depth² + offset²
+      correction = math.sqrt(math.pow(distance,2) - math.pow(depth,2))
+      print("Correction-offset:" + str(correction))
       
-   point.AddPoint(point.GetX()+UTMX, point.GetY()+UTMY)
-   point.Transform(transformback)
-   
-   
-   Lat=point.GetX()
-   Lon=point.GetY()
-   Accuracy=Accuracy
-   #Hier umwandeln in NMEA und ans Boot senden bitte David
-   #Bitte im Paket angeben statt fix DGPS
-  
-   time.sleep(0.5)
+      #Calc with compas in UTM
+      UTMY=math.sin(compass)*correction
+      UTMX=math.cos(compass)*correction
+      
+      #Angle 0 = cosinus 1 -- x
+      #Angle 90 = sinus 1 --> y
+      print("UTMX:" + str(UTMX))
+      print("UTMY:" + str(UTMY))
+      
+      # 
+      Lat="51.035540"
+      Lon="13.735870"
+      
+      # --> Bringe die Koordinaten in gdal
+      point.AddPoint(Lat, Lon)
+      #--> Transformiere in UTM
+      point.Transform(transform)
 
+      Accuracy = getAccuracy(distance, depth)
+
+      if RTK:
+         point.AddPoint(point.GetX()+RTKX, point.GetY()+RTKY)
+         Accuracy = Accuracy + 0,35
+      else:
+         Accuracy = Accuracy + 3
+         
+      point.AddPoint(point.GetX()+UTMX, point.GetY()+UTMY)
+      point.Transform(transformback)
+      
+      
+      Lat=point.GetX()
+      Lon=point.GetY()
+      Accuracy=Accuracy
+      #Hier umwandeln in NMEA und ans Boot senden bitte David
+      #Bitte im Paket angeben statt fix DGPS
+   
+      time.sleep(0.5)
+
+readSerialNMEA();
