092f1c3beabab8b08aa968c68984e27e17ebce6c
Bommel <m.bommhardt@gmail.com>
2023-06-30 00:49:02 +0200

    update

--- BOOT/Sensor2Mavlink/app/main.py
+++ BOOT/Sensor2Mavlink/app/main.py
@@ -231,7 +231,7 @@ class MM:
                 turbidity_calib2 = float(f.read())
                 
                 for channel in range(4):
-                    bus = SMBus(6)
+                    bus = SMBus(1)
                     ads_address= 0x48
                     config = [(0x84 | (channel << 4)), 0x83]
                     bus.write_i2c_block_data(ads_address, 0x01, config)
