f015eb41e564fc9d38ddde7c752dd7583b045c7e
Bommel <m.bommhardt@gmail.com>
2023-06-30 05:00:08 +0200

    update

--- BOOT/Sensor2Mavlink/app/main.py
+++ BOOT/Sensor2Mavlink/app/main.py
@@ -292,7 +292,8 @@ class MM:
                     global o2_ble_value
                     #Logoutput
                     print(str(o2_ble_value), str(round(list[1][0],3)), str(round(list[2][0]+(list[3][0]*1.75),3)), str(round(list[3][0],3)))                   
-                    self.send_sensors_to_mavlink(round(list[0][0],3),round(list[1][0],3),round(list[2][0],3),o2_ble_value)
+                    self.send_sensors_to_mavlink(str(o2_ble_value), str(round(list[1][0],3)), str(round(list[2][0]+(list[3][0]*1.75),3)), str(round(list[3][0],3)))
+                    #self.send_sensors_to_mavlink(round(list[0][0],3),round(list[1][0],3),round(list[2][0],3),o2_ble_value)
                     list=[],[],[],[] 
                     #print("Daten geschrieben")
                     #self.send_statustext("TEST")
