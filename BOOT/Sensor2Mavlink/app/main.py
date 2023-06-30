d2c2d66e535c3d9eca132223d8ae8bdfe54551fd
Bommel <m.bommhardt@gmail.com>
2023-06-30 04:00:25 +0200

    update

--- BOOT/Sensor2Mavlink/app/main.py
+++ BOOT/Sensor2Mavlink/app/main.py
@@ -139,7 +139,7 @@ class MM:
 
     def getO2(self) -> None:
         while True:
-                sleep(1.0)
+                time.sleep(1.0)
                 print("Starte O2")
                 try:
                     url = 'http://192.168.42.123/events'
